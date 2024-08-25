5d1918cc4b1982ba2d3d472c2168ea6dca2bf843
Rohan Pambhar <rohan@itagenturen.com>
2024-08-25 22:41:09 +0200

    Initial Commit

---
+++ FirebaseApp/FirebaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAppApp.swift
+//  FirebaseApp
+//
+//  Created by Rohan Pambhar on 2024-08-25.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
